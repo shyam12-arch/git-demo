4e227ad280012a445d0089daf9a2a98958e6a0ef
Shyam sundar Tyagi <shyamsundartyagi@Shyams-MacBook-Pro.local>
2026-03-17 12:11:28 +0530

    changes..

--- app.py
+++ app.py
@@ -3,8 +3,23 @@ print("Hello world.................")
 print("Hello world-2")
 print("feature branch created....")
 print("feature branch 2 commit")
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 1c235ae (add func add in feature branch)
+>>>>>>> 16cfb08 (changes..)
 print("3rd commit in feature branch")
 
 def add():
   print("addition...........")
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+>>>>>>> feature
+=======
+>>>>>>> 1c235ae (add func add in feature branch)
+>>>>>>> 16cfb08 (changes..)
